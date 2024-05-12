dcb17d39d9a9a2d5b54f570588bcf4f714609cb3
Jakub Bujak <kuba.bujak0@wp.pl>
2024-05-12 11:14:11 +0200

    utworzenie nowego użytkownika

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -2,6 +2,7 @@ package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,13 +33,13 @@ class UserController {
     }
 
     @PostMapping
-    public User addUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<User> addUser(@RequestBody UserDto userDto) {
 
         // Demonstracja how to use @RequestBody
         System.out.println("User with e-mail: " + userDto.email() + "passed to the request");
 
-        // TODO: saveUser with Service and return User
-        return null;
+        User addedUser = userService.createUser(userMapper.toEntity(userDto));
+        return ResponseEntity.ok().body(addedUser);
     }
 
 }
