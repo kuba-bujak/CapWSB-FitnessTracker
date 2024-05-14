0a39f5d40db49a6565c4f018881c1089cc73559e
Jakub Bujak <jakub.bujak@volvo.com>
2024-05-14 11:00:29 +0200

    Dodano poprawne statusy do response

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -2,6 +2,7 @@ package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,26 +21,24 @@ class UserController {
     private final BasicUserMapper basicUserMapper;
 
     @GetMapping
-    public List<BasicUserDto> getAllUsers() {
-        return userService.findAllUsers()
-                          .stream()
-                          .map(basicUserMapper::toDto)
-                          .toList();
+    public ResponseEntity<List<BasicUserDto>> getAllUsers() {
+        List<BasicUserDto> users = userService.findAllUsers().stream()
+                .map(basicUserMapper::toDto)
+                .toList();
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/user/{id}")
-    public Optional<User> getUserById(@PathVariable("id") final Long id) {
-        return userService.getUser(id);
+    public ResponseEntity<User> getUserById(@PathVariable("id") final Long id) {
+        Optional<User> user = userService.getUser(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody UserDto userDto) {
-
-        // Demonstracja how to use @RequestBody
-        System.out.println("User with e-mail: " + userDto.email() + "passed to the request");
-
         User addedUser = userService.createUser(userMapper.toEntity(userDto));
-        return ResponseEntity.ok().body(addedUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(addedUser);
     }
 
     @DeleteMapping("/user/{id}")
@@ -47,8 +46,10 @@ class UserController {
         Optional<User> userToDelete = userService.getUser(id);
         if (userToDelete.isPresent()) {
             userService.deleteUser(userToDelete.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok().body(userToDelete);
     }
 
 }
