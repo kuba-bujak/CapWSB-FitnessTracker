54fa74688683ce6910b015fcf1d0d9dc732f1b3c
Jakub Bujak <jakub.bujak@volvo.com>
2024-05-17 16:05:05 +0200

    Dodanie JavaDoc do Activity Type

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/ActivityType.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/ActivityType.java
@@ -1,20 +1,51 @@
 package com.capgemini.wsb.fitnesstracker.training.internal;
 
-// TODO : JavaDoc
+/**
+ * Enum representing different types of physical activities.
+ */
 public enum ActivityType {
 
+    /**
+     * Represents the activity of running.
+     */
     RUNNING("Running"),
+
+    /**
+     * Represents the activity of cycling.
+     */
     CYCLING("Cycling"),
+
+    /**
+     * Represents the activity of walking.
+     */
     WALKING("Walking"),
+
+    /**
+     * Represents the activity of swimming.
+     */
     SWIMMING("Swimming"),
+
+    /**
+     * Represents the activity of playing tennis.
+     */
     TENNIS("Tenis");
 
     private final String displayName;
 
+    /**
+     * Constructs an ActivityType with a display name.
+     *
+     * @param displayName the display name of the activity
+     */
     ActivityType(String displayName) {
         this.displayName = displayName;
     }
 
+    /**
+     * Gets the display name of the activity.
+     *
+     * @return the display name
+     */
     public String getDisplayName() {
         return displayName;
     }
